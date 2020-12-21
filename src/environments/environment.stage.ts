0dd68310f1cb42527c5e3d085ac7cf6a122f8e4f
Janos Vinceller <58742597+jvinceller@users.noreply.github.com>
2020-12-21 10:14:49 +0100

    Fix for heroku to start web server

---
+++ src/environments/environment.stage.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  name: "stage",
+  apiPath: "/stage/api",
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  name: "dev",
+  apiPath: "/api",
+};
